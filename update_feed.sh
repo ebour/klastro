0c96d6deb1fdaa69db12c457ead5f22ea085649b
ebour <ebour74@gmail.com>
2024-03-10 16:04:14 +0100

    Update update_feed.sh

--- update_feed.sh
+++ update_feed.sh
@@ -2,6 +2,7 @@
 
 python -m main
 
+git remote add origin https://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@github.com/ebour/klastro
 git pull
 git add klastro.atom
 git commit -m "Update astronomical forecast"
